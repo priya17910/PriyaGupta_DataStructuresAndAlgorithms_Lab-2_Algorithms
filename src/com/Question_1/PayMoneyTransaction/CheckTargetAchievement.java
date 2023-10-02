4769abb7efa88996e5a0c93053811597d8dffc77
priya17910 <priyagupta17910@gmail.com>
2023-10-02 23:03:55 +0530

    Update CheckTargetAchievement.java Indentation

--- src/com/Question_1/PayMoneyTransaction/CheckTargetAchievement.java
+++ src/com/Question_1/PayMoneyTransaction/CheckTargetAchievement.java
@@ -6,26 +6,24 @@ public class CheckTargetAchievement {
 	public void checkTargetAchieved (int[] transactions, int targetValue)
 	{
 		// TRANSACTION COUNT INDICATES THE NUMBER OF TRANSACTIONS REQUIRED TO ACHEIVE THE TARGET
-        	int transactionCount = 0;
-        
-        
-        	/* LOOP TO TRAVERSE THE TRANSACTION ARRAY STARTS HERE */
-        	for (int i = 0; i < transactions.length; i++) 
-		{
-        		targetValue -= transactions[i];
-            		transactionCount++;
-
-            		// CODE TO DISPLAY NUMBER OF TRANSACTIONS
-            		if (targetValue <= 0) {
-                		System.out.println("Target achieved after " + transactionCount + " transactions");
-                		break;
-            		}
-        	}
-        	/* LOOP TO TRAVERSE THE TRANSACTION ARRAY ENDS HERE */
-
-        	// CHECKING THIS CONDITION TO KNOW WHETHER TARGET CAN BE ACHIEVED OR NOT
-        	if (targetValue > 0) {
-            		System.out.println("Given target is not achieved");
-        	}
+		int transactionCount = 0;
+		
+		/* LOOP TO TRAVERSE THE TRANSACTION ARRAY STARTS HERE */
+		for (int i = 0; i < transactions.length; i++) {
+			targetValue -= transactions[i];
+			transactionCount++;
+			
+			// CODE TO DISPLAY NUMBER OF TRANSACTIONS
+			if (targetValue <= 0) {
+				System.out.println("Target achieved after " + transactionCount + " transactions");
+				break;
+			}
+		}
+		/* LOOP TO TRAVERSE THE TRANSACTION ARRAY ENDS HERE */
+		
+		// CHECKING THIS CONDITION TO KNOW WHETHER TARGET CAN BE ACHIEVED OR NOT
+		if (targetValue > 0) {
+			System.out.println("Given target is not achieved");
+		}
 	}
 }
